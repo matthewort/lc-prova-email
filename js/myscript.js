eb7c495e952ec5938699cd00b6eea3061ed6c0cd
Matteo Ferrarese <matteo.ferrarese@yahoo.it>
2020-10-23 10:48:05 +0200

    Prova esercizio e-mail

---
+++ js/myscript.js
@@ -0,0 +1,29 @@
+// Mail
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+// richiesta mail
+var richiesta = prompt("inserisci la mail");
+// lista mail
+var email = ["marco@gmail.com", "giulia@hotmail.it", "marta@yahoo.it", "giorgio@gmail.it"];
+console.log(richiesta);
+
+var emailTrovata = false;
+
+for (var i = 0; i < email.length; i++) {
+  var compilazione = email[i];
+  // condizione mail corretta o errata
+  if (compilazione === richiesta) {
+    emailTrovata = true;
+  //   console.log("Benvenuto!");
+  // } else {
+  //   console.log("Accesso negato");
+  }
+}
+
+if (emailTrovata === true) {
+  console.log("Benvenuto!");
+} else {
+  console.log("Accesso negato");
+}
